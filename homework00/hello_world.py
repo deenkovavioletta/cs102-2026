26e0604fff3ee9fc643f980292f89045e148239f
deenkovavioletta <deenkovavioletta@gmail.com>
2026-09-21 10:54:18 +0300

    dideverithigright

--- homework00/hello_world.py
+++ homework00/hello_world.py
@@ -5,29 +5,28 @@ def text():
     """Return a greeting."""
     return "message"
 
+
 ######## cложение
 # def algo():
 #     return sum(list(map(int, input().split())))
 
 
-def algo1():
-    s = input() # "100 88"
-    list_of_two_string_numbers = s.split() # ["100", "88"]
-    list_of_two_numbers = list(map(int, list_of_two_string_numbers)) # [100, 88]
-    a, b = list_of_two_numbers
-    return a + b 
-
-def algo2():
-    s = input() # "100 88"
-    list_of_two_string_numbers = s.split() # ["100", "88"]
-    list_of_two_numbers = list(map(int, list_of_two_string_numbers)) # [100, 88]
-    a, b = list_of_two_numbers
-    return a + b*b
-
-if __name__ == "__main__":
-    # print(text())
-    print(algo2())
+# def algo1():
+#     s = input()  # "100 88"
+#     list_of_two_string_numbers = s.split()  # ["100", "88"]
+#     list_of_two_numbers = list(map(int, list_of_two_string_numbers))  # [100, 88]
+#     a, b = list_of_two_numbers
+#     return a + b
 
 
+# def algo2():
+#     s = input()  # "100 88"
+#     list_of_two_string_numbers = s.split()  # ["100", "88"]
+#     list_of_two_numbers = list(map(int, list_of_two_string_numbers))  # [100, 88]
+#     a, b = list_of_two_numbers
+#     return a + b * b
 
 
+# if __name__ == "__main__":
+#     # print(text())
+#     print(algo2())

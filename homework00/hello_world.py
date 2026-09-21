cd26dd0f12433afdeb203d7827e93f324166cf33
deenkovavioletta <deenkovavioletta@gmail.com>
2026-09-21 10:18:02 +0300

    fixed hello_word method output

--- homework00/hello_world.py
+++ homework00/hello_world.py
@@ -3,8 +3,31 @@
 
 def text():
     """Return a greeting."""
-    return "Hello, World!"
+    return "message"
 
+######## cложение
+# def algo():
+#     return sum(list(map(int, input().split())))
+
+
+def algo1():
+    s = input() # "100 88"
+    list_of_two_string_numbers = s.split() # ["100", "88"]
+    list_of_two_numbers = list(map(int, list_of_two_string_numbers)) # [100, 88]
+    a, b = list_of_two_numbers
+    return a + b 
+
+def algo2():
+    s = input() # "100 88"
+    list_of_two_string_numbers = s.split() # ["100", "88"]
+    list_of_two_numbers = list(map(int, list_of_two_string_numbers)) # [100, 88]
+    a, b = list_of_two_numbers
+    return a + b*b
 
 if __name__ == "__main__":
-    print(text())
+    # print(text())
+    print(algo2())
+
+
+
+
